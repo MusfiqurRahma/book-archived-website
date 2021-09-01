d89ed514722e36e4004c1cb1908413929dd5c582
Musfiqur Rahma <musfiq371@gmail.com>
2021-09-02 03:02:04 +0600

    modify

--- api.js
+++ api.js
@@ -8,11 +8,26 @@ const searchField = () => {
         .then(res => res.json())
         .then(data=>displayBook(data.docs))
 }
+    //  display all books
      const displayBook = (docs) => {
      const imageBox = document.getElementById('image-box');
     // console.log(books);
          docs.forEach(book => {
          console.log(book);
+         const div = document.createElement('div');
+             div.classList.add('col');
+             div.innerHTML = `
+             <div class="card">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Book Name: ${book.title}</h5>
+              <h6 class="card-text">Author: ${book.author_name}</h6>
+              <h6 class="card-text">Publisher: ${book.publisher}</h6>
+              <p class="card-text">First Publish: ${book.first_publish_year}</p>
+            </div>
+          </div>
+             `
+             imageBox.appendChild(div);
      })
 }
 
