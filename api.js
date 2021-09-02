e120e4cdf95fd2997835e0e56e3853c200aed981
Musfiqur Rahma <musfiq371@gmail.com>
2021-09-02 18:21:57 +0600

    small modify

--- api.js
+++ api.js
@@ -14,10 +14,18 @@ const searchField = () => {
         .then(data => displayBook(data))
 
 }
-    //  display all books and make dynamic
+    //  display books
 const displayBook = (books) => {
          const imageBox = document.getElementById('image-box');
          imageBox.textContent = '';
+  // found data result
+  const foundData = document.getElementById('found-data');
+  foundData.textContent = '';
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <h4> Found Data: ${books.numFound} </h4>`
+  foundData.appendChild(div);
+  
   const errorDiv = document.getElementById('error');
   errorDiv.style.color = 'red';
   errorDiv.style.fontSize = '20px';
@@ -26,24 +34,18 @@ const displayBook = (books) => {
          if (books.length === 0) {
             errorDiv.innerText ='No Matching Found'
   }
-  const foundData = document.getElementById('found-data');
-  foundData.textContent = '';
-  const div = document.createElement('div')
-  div.innerHTML = `
-  <h4> Found Data: ${books.numFound} </h4>`
-  foundData.appendChild(div);
 
-  // console.log(books.numFound);
+          // display details about books
            books.docs.forEach(book => {
             const div = document.createElement('div');
              div.classList.add('col');
              div.innerHTML = `
-             <div class="card">
+             <div class="card w-75 h-100">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Book Name: ${book.title}</h5>
               <h6 class="card-text">Author: ${book.author_name}</h6>
-              <h6 class="card-text">Publisher: ${book.publisher}</h6>
+              <h6 class="card-text">Publisher: ${book.publisher.slice(0,2)}</h6>
               <p class="card-text">First Publish: ${book.first_publish_year}</p>
             </div>
           </div>
